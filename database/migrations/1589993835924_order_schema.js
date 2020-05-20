66057ecb50cb793ec488176c65b1cf4f3598d694
Juma Allan <allanjuma@gmail.com>
2020-05-20 20:37:32 +0300

    updated the database migrations

--- database/migrations/1589993835924_order_schema.js
+++ database/migrations/1589993835924_order_schema.js
@@ -7,10 +7,10 @@ class OrderSchema extends Schema {
   up () {
     this.create('orders', (table) => {
       table.increments()
-      table.string('title').nullable()
-      table.string('isbn').nullable()
-      table.string('publisher_name').nullable()
-      table.string('author_name').nullable()
+      table.string('product').nullable()
+      table.string('name').nullable()
+      table.string('price').nullable()
+      table.string('amount').nullable()
       table.timestamps()
     })
   }
