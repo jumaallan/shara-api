c3769ef8eb33ecd78ab221753ff7f48cf3956b39
Juma Allan <allanjuma@gmail.com>
2020-05-20 20:37:57 +0300

    setting up the Order Controller

---
+++ app/Controllers/Http/OrderController.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const Order = use('App/Models/Order')
+
+class OrderController {
+
+  async index({response}) {
+    let orders = await Order.all()
+    return response.json(orders)
+  }
+
+  async show({params, response}) {
+    const order = await Order.find(params.id)
+    return response.json(order)
+  }
+
+  async store({request, response}) {
+    const orderInfo = request.only(['product', 'name', 'price', 'amount'])
+
+    const order = new Order()
+    order.product = orderInfo.product
+    order.name = orderInfo.name
+    order.price = orderInfo.price
+    order.amount = orderInfo.amount
+
+    await order.save()
+
+    return response.status(201).json(order)
+  }
+
+  async update({params, request, response}) {
+    const orderInfo = request.only(['product', 'name', 'price', 'amount'])
+
+    const order = await Order.find(params.id)
+    if (!order) {
+      return response.status(404).json({data: 'Order not found'})
+    }
+    order.product = orderInfo.product
+    order.name = orderInfo.name
+    order.price = orderInfo.price
+    order.amount = orderInfo.amount
+
+    await order.save()
+
+    return response.status(200).json(order)
+  }
+
+  async delete({params, response}) {
+    const order = await Order.find(params.id)
+    if (!order) {
+      return response.status(404).json({data: 'Order not found'})
+    }
+    await order.delete()
+
+    return response.status(204).json(null)
+  }
+}
+
+module.exports = OrderController
