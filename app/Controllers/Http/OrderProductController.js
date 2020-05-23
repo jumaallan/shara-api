2efa07e9de6fc7f8f7d1c2059133f8ac4dd72c1f
Juma Allan <allanjuma@gmail.com>
2020-05-24 00:09:13 +0300

    fetch orders, and products per user id <> from bearer

--- app/Controllers/Http/OrderProductController.js
+++ app/Controllers/Http/OrderProductController.js
@@ -2,8 +2,18 @@
 
 const Order = use('App/Models/Order')
 const Product = use('App/Models/Product')
+const User = use('App/Models/User')
 
 class OrderProductController {
+
+  async show({auth, response}) {
+
+    const user = await User.find(auth.current.user.id)
+
+    let orders = await Order.all()
+    return response.json(user)
+  }
+
 }
 
 module.exports = OrderProductController
