a1dbc24bec2f8171008f5f8470e54c8487406f76
Juma Allan <allanjuma@gmail.com>
2020-05-23 20:51:19 +0300

    setting up the product model

---
+++ database/migrations/1590256192706_product_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up() {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('order_id').nullable()
+      table.string('name').nullable()
+      table.string('price').nullable()
+      table.string('uom').nullable()
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('products')
+  }
+}
+
+module.exports = ProductSchema
