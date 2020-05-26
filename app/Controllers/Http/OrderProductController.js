c24422665e3d7b8b76616a932177fd3af2159bb6
Juma Allan <allanjuma@gmail.com>
2020-05-27 02:34:25 +0300

    updated order products controller

--- app/Controllers/Http/OrderProductController.js
+++ app/Controllers/Http/OrderProductController.js
@@ -8,12 +8,21 @@ class OrderProductController {
 
   async show({auth, response}) {
 
-    const user = await User.find(auth.current.user.id)
-
     let orders = await Order.all()
-    return response.json(user)
+    return response.json(orders)
   }
 
 }
 
 module.exports = OrderProductController
+
+
+  [
+  {
+    "id": 1,
+    "user_id": 1,
+    "order_number": "0001",
+    "created_at": "2020-05-26 22:23:14",
+    "updated_at": "2020-05-26 22:23:14"
+  }
+  ]
